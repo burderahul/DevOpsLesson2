d925f7e1bf107923d1b7691419ac003013b7bf3b
Ubuntu <rahul.burde@yahoo.co.in>
2019-08-17 13:59:44 +0000

    added next files

---
+++ hellojava.java
@@ -0,0 +1,6 @@
+class hellojava{
+public static void main(string args[]){
+System.out.Println("Hello I am java program, Thank you !");
+}
+}
+
---
+++ index.js
@@ -0,0 +1,2 @@
+var texts= "I am javasciprt program, Thank you !";
+console.log(tests);
